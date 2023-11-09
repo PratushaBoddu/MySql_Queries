f4a23752d99a495eaee1cff17680aa13f964e932
PratushaBoddu <150272501+PratushaBoddu@users.noreply.github.com>
2023-11-09 10:31:42 +0000

    Update MySql_queries.sql

--- MySql_queries.sql
+++ MySql_queries.sql
@@ -848,23 +848,7 @@ INSERT INTO `products` VALUES ('S10_1678','1969 Harley Davidson Ultimate Chopper
 
 
 
---
--- Table structure for table `orderdetails`
---
 
-DROP TABLE IF EXISTS `orderdetails`;
-
-CREATE TABLE `orderdetails` (
-  `orderNumber` int(11) NOT NULL,
-  `productCode` varchar(15) NOT NULL,
-  `quantityOrdered` int(11) NOT NULL,
-  `priceEach` decimal(10,2) NOT NULL,
-  `orderLineNumber` smallint(6) NOT NULL,
-  PRIMARY KEY (`orderNumber`,`productCode`),
-  KEY `productCode` (`productCode`),
-  CONSTRAINT `orderdetails_ibfk_1` FOREIGN KEY (`orderNumber`) REFERENCES `orders` (`orderNumber`),
-  CONSTRAINT `orderdetails_ibfk_2` FOREIGN KEY (`productCode`) REFERENCES `products` (`productCode`)
-) ;
 
 
 
